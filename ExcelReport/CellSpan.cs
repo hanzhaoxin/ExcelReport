623f658837bc6600a209dee36a2733ea730e6b80
hanzhaoxin <hanzhaoxin@163.com>
2015-01-23 22:55:56 +0800

    Add the image processing.
    添加对图像的处理。

---
+++ ExcelReport/CellSpan.cs
@@ -0,0 +1,32 @@
+﻿/*
+ 类：CellSpan
+ 描述：描述单元格的跨度
+ 编 码 人：韩兆新 日期：2015年01月23日
+ 修改记录：
+
+*/
+namespace ExcelReport
+{
+    struct CellSpan
+    {
+        public CellSpan(int rowSpan, int colSpan)
+        {
+            this.rowSpan = rowSpan;
+            this.colSpan = colSpan;
+        }
+        private int rowSpan;
+
+        public int RowSpan
+        {
+            get { return rowSpan; }
+            set { rowSpan = value; }
+        }
+        private int colSpan;
+
+        public int ColSpan
+        {
+            get { return colSpan; }
+            set { colSpan = value; }
+        }
+    }
+}
---
+++ ExcelReport/Extend/ICellExtend.cs
@@ -0,0 +1,34 @@
+﻿/*
+ 类：ICellExtend
+ 描述：为ICell扩展方法
+ 编 码 人：韩兆新 日期：2015年01月23日
+ 修改记录：
+
+*/
+using NPOI.SS.UserModel;
+
+namespace ExcelReport
+{
+    static class ICellExtend
+    {
+        public static CellSpan GetSpan(this ICell cell)
+        {
+            var cellSpan = new CellSpan(1,1);
+            if (cell.IsMergedCell)
+            {
+                int regionsNum = cell.Sheet.NumMergedRegions;
+                for (int i = 0; i < regionsNum; i++)
+                {
+                    var range = cell.Sheet.GetMergedRegion(i);
+                    if (range.FirstRow == cell.RowIndex && range.FirstColumn == cell.ColumnIndex)
+                    {
+                        cellSpan.RowSpan = range.LastRow - range.FirstRow + 1;
+                        cellSpan.ColSpan = range.LastColumn - range.FirstColumn + 1;
+                        break;
+                    }
+                }
+            }
+            return cellSpan;
+        }
+    }
+}
